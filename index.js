cd83d516ae5bb973671603c3d92629de30e4103d
Craig Neff <ifly12610@gmail.com>
2021-02-08 16:40:39 -0700

    1a-c DONE - Started working on question 2

--- index.js
+++ index.js
@@ -6,8 +6,22 @@ var lastNum = (ages[ages.length - 1]);
 var math = lastNum - ages[0];
 console.log(math);
 
-// ages.push(50);
+ages.push(50);
 
-// var lastNum = (ages[ages.length - 1]);
-// var math = lastNum - ages[0];
-// console.log(math);
+var lastNum = (ages[ages.length - 1]);
+var math = lastNum - ages[0];
+console.log(math);
+
+
+
+var ages = [3, 9, 23, 64, 2, 8, 28, 93];
+
+let sum = 0
+for (var i = 0; i <= ages.length - 1; i++) {
+    // console.log(ages[i])
+    sum += ages[i];
+}
+console.log(sum); //All ages added up 
+console.log(sum / ages.length); //Sum divided by the length of the ages array 
+
+var names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
