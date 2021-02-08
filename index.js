2cf9a4870dd715154510aee88d84f33374426beb
Craig Neff <ifly12610@gmail.com>
2021-02-08 14:44:07 -0700

    Starting working on solutions for coding assigment..1b done.

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var ages = [3, 9, 23, 64, 2, 8, 28, 93];
+
+var lastNum = (ages[ages.length - 1]);
+var math = lastNum - ages[0];
+console.log(math);
+
+// ages.push(50);
+
+// var lastNum = (ages[ages.length - 1]);
+// var math = lastNum - ages[0];
+// console.log(math);
