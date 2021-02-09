b8c8d60909ae254dbeea74be53ab0f2a632b7a49
Craig Neff <ifly12610@gmail.com>
2021-02-08 21:36:01 -0700

    working on 2b

--- index.js
+++ index.js
@@ -24,4 +24,19 @@ for (var i = 0; i <= ages.length - 1; i++) {
 console.log(sum); //All ages added up 
 console.log(sum / ages.length); //Sum divided by the length of the ages array 
 
+
+
 var names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+
+let i = 0;
+
+let namesSum = 0;
+while (i < names.length) {
+    let singName = names[i];
+    namesSum += singName.length;
+    i++;
+} console.log(namesSum / names.length);
+
+var names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+
+for (var i = 0; i < names.length; i++);
