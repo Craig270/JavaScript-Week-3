6ebb8882f8364ad57d32b83db980b9ec38131b47
Craig Neff <ifly12610@gmail.com>
2021-02-09 20:56:19 -0700

    on question 7

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 'use strict'
 
-// var ages = [3, 9, 23, 64, 2, 8, 28, 93];
+var ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
-// var lastNum = (ages[ages.length - 1]);
-// var math = lastNum - ages[0];
-// console.log(math);
+var lastNum = (ages[ages.length - 1]);
+var math = lastNum - ages[0];
+console.log(math);
 
-// ages.push(50);
+ages.push(50);
 
-// var lastNum = (ages[ages.length - 1]);
-// var math = lastNum - ages[0];
-// console.log(math);
-
-
-
-// var ages = [3, 9, 23, 64, 2, 8, 28, 93];
-
-// let sum = 0
-// for (var i = 0; i <= ages.length - 1; i++) {
-//     console.log(ages[i])
-//     sum += ages[i];
-// }
-// console.log(sum); //All ages added up 
-// console.log(sum / ages.length); //Sum divided by the length of the ages array 
+var lastNum = (ages[ages.length - 1]);
+var math = lastNum - ages[0];
+console.log(math);
 
 
 
-// var names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+var ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
-// let i = 0;
+let sum = 0
+for (var i = 0; i <= ages.length - 1; i++) {
+    console.log(ages[i])
+    sum += ages[i];
+}
+console.log(sum); //All ages added up 
+console.log(sum / ages.length); //Sum divided by the length of the ages array 
 
-// let namesSum = 0;
-// while (i < names.length) {
-//     let singName = names[i];
-//     namesSum += singName.length;
-//     i++;
-// } console.log(namesSum / names.length);
+
+
+var names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+
+let i = 0;
+
+let namesSum = 0;
+while (i < names.length) {
+    let singName = names[i];
+    namesSum += singName.length;
+    i++;
+} console.log(namesSum / names.length);
 
 var names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
 
@@ -49,10 +49,18 @@ for (var i = 0; i <= names.length - 1; i++) {
 console.log(bigNamesList)
 
 
+var nameLengths = [];
 
+for (var i = 0; i <= names.length - 1; i++) {
+    nameLengths.push(names[i].length);
+};
+console.log(nameLengths);
 
+var nameLengthSum = 0;
 
-
+for (var i = 0; i < nameLengths.length; i++) {
+    nameLengthSum += nameLengths[i];
+} console.log(nameLengthSum);
 
 
 
