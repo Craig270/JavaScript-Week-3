51b6643c9d83d4ac0a95b3f90ed47c9c7f6ae090
Craig Neff <ifly12610@gmail.com>
2021-02-09 20:11:58 -0700

    finished more coding

--- index.js
+++ index.js
@@ -1,42 +1,97 @@
 'use strict'
 
-var ages = [3, 9, 23, 64, 2, 8, 28, 93];
+// var ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
-var lastNum = (ages[ages.length - 1]);
-var math = lastNum - ages[0];
-console.log(math);
+// var lastNum = (ages[ages.length - 1]);
+// var math = lastNum - ages[0];
+// console.log(math);
 
-ages.push(50);
+// ages.push(50);
 
-var lastNum = (ages[ages.length - 1]);
-var math = lastNum - ages[0];
-console.log(math);
+// var lastNum = (ages[ages.length - 1]);
+// var math = lastNum - ages[0];
+// console.log(math);
 
 
 
-var ages = [3, 9, 23, 64, 2, 8, 28, 93];
+// var ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
-let sum = 0
-for (var i = 0; i <= ages.length - 1; i++) {
-    // console.log(ages[i])
-    sum += ages[i];
+// let sum = 0
+// for (var i = 0; i <= ages.length - 1; i++) {
+//     console.log(ages[i])
+//     sum += ages[i];
+// }
+// console.log(sum); //All ages added up 
+// console.log(sum / ages.length); //Sum divided by the length of the ages array 
+
+
+
+// var names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+
+// let i = 0;
+
+// let namesSum = 0;
+// while (i < names.length) {
+//     let singName = names[i];
+//     namesSum += singName.length;
+//     i++;
+// } console.log(namesSum / names.length);
+
+var names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+
+var bigNamesList = " ";
+
+for (var i = 0; i <= names.length - 1; i++) {
+    bigNamesList += names[i] + " ";
+
+};
+
+console.log(bigNamesList)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(" ")
+console.log(" ")
+console.log(" ")
+console.log(" ")
+console.log(" ")
+console.log(" ")
+console.log(" ")
+console.log(" ")
+console.log(`Everything below this line is test`);
+
+var bigNames = ["Craig", "Jennifer", "Holden", "Moxie"];
+var smallNames = " ";
+
+
+for (let i = 0; i < bigNames.length - 1; i++) {
+    console.log(bigNames[i]);
 }
-console.log(sum); //All ages added up 
-console.log(sum / ages.length); //Sum divided by the length of the ages array 
 
+bigNames[0] + smallNames;
 
-
-var names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
-
-let i = 0;
-
-let namesSum = 0;
-while (i < names.length) {
-    let singName = names[i];
-    namesSum += singName.length;
-    i++;
-} console.log(namesSum / names.length);
-
-var names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
-
-for (var i = 0; i < names.length; i++);
+console.log(smallNames);
